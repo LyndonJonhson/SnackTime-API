1bc4b2552796016dac5a93ae0eee307bcc516619
Jonhson Cabral <jonhsoncabral13@gmail.com>
2022-04-12 11:29:26 -0300

    deploy

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const database = require('knex')({
     client: 'mysql2',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: '020896130597',
-        database: 'snacktime'
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'bef1363787ccc7',
+        password: '6c514baf',
+        database: 'heroku_8abdd38c7d33819'
     }
 });
 
